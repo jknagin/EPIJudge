356e1621a94e501b3991d005a8b0689ab2ce07ba
Jay Nagin <jknagin@gmail.com>
2021-06-09 15:38:08 -0700

    5.10

--- epi_judge_cpp/apply_permutation.cc
+++ epi_judge_cpp/apply_permutation.cc
@@ -3,7 +3,20 @@
 #include "test_framework/generic_test.h"
 using std::vector;
 void ApplyPermutation(vector<int> perm, vector<int>* A_ptr) {
-  // TODO - you fill in here.
+  std::vector<int>& A = *A_ptr;
+  for (int i = 0; i < perm.size(); ++i)
+  {
+    // We would like perm to be like [0, 1, 2, 3], so there is no more permuting to do
+    // While this is not the case:
+    while (perm[i] != i)
+    {
+      // Put the i'th element of A in the correct position (perm[i])
+      std::swap(A[i], A[perm[i]]);
+
+      // Perform the swap in the perm array to record the swap we just did in the A array
+      std::swap(perm[i], perm[perm[i]]);
+    }
+  }
   return;
 }
 vector<int> ApplyPermutationWrapper(const vector<int>& perm, vector<int> A) {
