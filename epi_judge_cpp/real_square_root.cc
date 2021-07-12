0012f35d49c407694ae3269290d45263d0ddee70
Jay Nagin <jknagin@gmail.com>
2021-07-12 10:36:13 -0700

    11.5

--- epi_judge_cpp/real_square_root.cc
+++ epi_judge_cpp/real_square_root.cc
@@ -1,8 +1,43 @@
 #include "test_framework/generic_test.h"
 
+double square(double x) { return x * x; }
+
+int compare(const double& a, const double& b)
+{
+  double diff = (a - b) / std::max(std::abs(a), std::abs(b)); // normalized difference
+  if (diff < -std::numeric_limits<double>::epsilon()) return -1;
+  if (diff > std::numeric_limits<double>::epsilon()) return 1;
+  return 0;
+}
+
 double SquareRoot(double x) {
-  // TODO - you fill in here.
-  return 0.0;
+  // sqrt(x) > x for x in (0, 1), so lower and upper bounds are computed differently in this case
+  double left = x < 1 ? 0 : 1;
+  double right = x < 1 ? 1 : x;
+
+  while(compare(left, right) == -1)
+  {
+    double mid = (right - left) / 2 + left;
+    double sqmid = square(mid);
+    int cmp = compare(sqmid, x);
+    switch (cmp)
+    {
+    case 0:
+      return mid;
+      break;
+    case 1:
+      right = mid;
+      break;
+    case -1:
+      left = mid;
+      break; 
+    default:
+      break;
+    }
+  }
+
+  // left >= right, so return either left or right
+  return right;
 }
 
 int main(int argc, char* argv[]) {
