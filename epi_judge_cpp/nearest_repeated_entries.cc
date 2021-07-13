4739e087b8ea06ae5d0da98f02d4bb6a5cd16dd3
Jay Nagin <jknagin@gmail.com>
2021-07-13 09:49:46 -0700

    12.5

--- epi_judge_cpp/nearest_repeated_entries.cc
+++ epi_judge_cpp/nearest_repeated_entries.cc
@@ -1,13 +1,24 @@
 #include <string>
 #include <vector>
+#include <unordered_map>
 
 #include "test_framework/generic_test.h"
 using std::string;
 using std::vector;
 
-int FindNearestRepetition(const vector<string>& paragraph) {
-  // TODO - you fill in here.
-  return 0;
+int FindNearestRepetition(const vector<string>& paragraph)
+{
+  int minDistance = std::numeric_limits<int>::max();
+  std::unordered_map<std::string, int> indexOf;
+  for(int i=0;i<paragraph.size();++i)
+  {
+    if (indexOf.count(paragraph[i]))
+    {
+      minDistance = std::min(minDistance, i - indexOf[paragraph[i]]);
+    }
+    indexOf[paragraph[i]] = i;
+  }
+  return minDistance != std::numeric_limits<int>::max() ? minDistance : -1;
 }
 
 int main(int argc, char* argv[]) {
