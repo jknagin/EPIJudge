16095a7b1a1f4f652a19867291040c42b687d881
Jay Nagin <jknagin@gmail.com>
2021-07-15 15:06:29 -0700

    16.4

--- epi_judge_cpp/binomial_coefficients.cc
+++ epi_judge_cpp/binomial_coefficients.cc
@@ -1,8 +1,30 @@
 #include "test_framework/generic_test.h"
 
+/*
+Recursive method (top-down DP) for compute N choose K without worrying about overflow from computing large factorials.
+This is REALLY useful!
+Base cases:
+  C(r, r) = 0
+  C(r, 0) = 0
+Recursion:
+  C(n, k) = C(n - 1, k) + C(n - 1, k - 1)
+*/
+int NChooseK(int n, int k, std::vector<std::vector<int> >& cache)
+{
+  if (cache[n][k]) return cache[n][k];
+  if (n == k || k == 0)
+  {
+    cache[n][k] = 1;
+    return cache[n][k];
+  }
+  int x = NChooseK(n - 1, k, cache);
+  int y = NChooseK(n - 1, k - 1, cache);
+  cache[n][k] = x + y;
+  return cache[n][k];
+}
 int ComputeBinomialCoefficient(int n, int k) {
-  // TODO - you fill in here.
-  return 0;
+  std::vector<std::vector<int> >cache(n + 1, std::vector<int>(k + 1, 0));
+  return NChooseK(n, k, cache);
 }
 
 int main(int argc, char* argv[]) {
